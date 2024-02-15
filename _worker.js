44478a4c288e63c309bda4ea29d8a45ac63f79b9
mehrsad20 <160099135+mehrsad20@users.noreply.github.com>
2024-02-15 09:06:54 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,8 @@ import { connect } from 'cloudflare:sockets';
 // t.me/P_tech2024 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '2cd61524-9843-43ab-8a0a-df4a744bb859';
+
+let userID = 'bd26c227-6fd0-48dd-9ed5-6d620ff860c3';
 
 const proxyIPs = ['mtn.ircf.space', 'mkh.ircf.space', 'mci.ircf.space', 'rtl.ircf.space'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
